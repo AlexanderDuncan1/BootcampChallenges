15effa7dfcd36b0b32eda0ae0a76aa6fb568209e
AlexanderDuncan1 <hellstern@gmail.com>
2023-06-22 21:20:36 +1000

    week4_challenge

--- README.md
+++ README.md
@@ -1,113 +1,22 @@
-# Week 3 Challenge - Password Generator.
+# Code Quiz
 
-## Github Link 
-https://github.com/AlexanderDuncan1/BootcampChallenges
+## Your Task
+In this project i atempted to make a quiz that filled the following user criteria
 
-## Site Link.
-https://alexanderduncan1.github.io/BootcampChallenges/
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
 
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
 
-## Authors notes.
-Week 3's challenge was to write a script to generate a passoword for a fictitious employee who has access to sensitive data and requires a random password that meets their specified requirements to help increase their secutiry.
-
-After making the generator create a password based on the users requirement i wanted to add a button to copy their password to make the users experience more stream lined. I also wanted to do this to try and see if i could add a button to the webpage without changing its layout regardless of the size or device of the browser.
-
-While doing this i found a few new commands in java relating the text selection and element seletion (which we ended up covering in the following weeks class) Which i found helped me alot in the following classes in week 4.
-
-
-
-
-## User Story
-
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
-
-## Acceptance Criteria
-
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
-# <Your-Project-Title>
-
-## Description
-
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-
-## Installation
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
-## Usage
-
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-## License
-
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+I tried to make the quiz run with as little user input as possible to create a more streamlined experiece, unfortunatly i did not get to add as much styling as i would have liked but I didnt manage to get to it.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,246 @@
-const generateBtn = document.querySelector("#generate");
-const copyBtn = document.querySelector("#copy");
+const quizData = [
+  {question: "Welcome",
+  answers: [
+    "Start"],
+  correctAnswer: 0
+},
+{question: "Is this Fun?",
+answers: [
+  "Very Fun",
+  "Not Fun",],
+correctAnswer: 0
+},
+{question: "Is JavaScript Cool?",
+answers: [
+  "No"],
+correctAnswer: 0
+},
+{question: "That does CSS stand for",
+answers: [
+  "Code Strict Systems",
+  "Cascade Super Strength",
+  "Cascading Style Sheets",
+  "Container Securtiy Systems",],
+correctAnswer: 2
+},
+{question: "What does API stand for?",
+answers: [
+  "Application Programming Interface",
+  "Attention Programming Intergration",
+  "Applicable Paramater Intigration",
+  "Animal Petting Institute"],
+correctAnswer: 0
+},
+{question: "I ran out of time, sorry",
+answers: [
+  "Make more time for the next challenge",
+  "Drink more coffee",
+  "Try harder",
+  "Copy, Paste"],
+correctAnswer: 0
+},
+{question: "Question 6",
+answers: [
+  "Answer 1",
+  "Answer 2",
+  "Answer 3",
+  "Correct Answer"],
+correctAnswer: 3
+},
+{question: "Question 7",
+answers: [
+  "Answer 1",
+  "Answer 2",
+  "Correct Answer",
+  "Answer 4"],
+correctAnswer: 2
+},
+{question: "Question 8",
+answers: [
+  "Answer 1",
+  "Correct Answer",
+  "Answer 3",
+  "Answer 4"],
+correctAnswer: 1
+},
+{question: "Question 9",
+answers: [
+  "Correct Answer",
+  "Answer 2",
+  "Answer 3",
+  "Answer 4"],
+correctAnswer: 0
+},
+{question: "Your guess is as good as mine",
+answers: [
+  "Answer 1",
+  "Answer 2",
+  "Answer 3",
+  "Answer 4"],
+correctAnswer: 2
+},]
+       
+const questionElement = document.getElementById("question");
+const choicesElement = document.getElementById("choices-container");
+const resultElement = document.getElementById("result");
+const scoreElement = document.getElementById("score");
+const timerElement = document.getElementById("timer");
 
-function writePassword() {
-  const passwordLength = parseInt(prompt("Enter desired password length (between 8 and 128 characters):"));
+let currentQuestionIndex = 0;
+let score = 0;
+let timer;
+let timeLeft = 60;
+let answerSubmitted = false;
+let timerRunning = false;
 
-  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    alert("Not a valid response. Select between 8 and 128.");
-    return;
+function initializeQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  timeLeft = 60;
+  answerSubmitted = false;
+  showQuestion();
+}
+
+function showQuestion() {
+  const currentQuestion = quizData[currentQuestionIndex];
+  questionElement.textContent = currentQuestion.question;
+
+  choicesElement.innerHTML = "";
+
+  currentQuestion.answers.forEach((answer, index) => {
+    const button = document.createElement("button");
+    button.classList.add("choice");
+    button.textContent = answer;
+    button.dataset.answerIndex = index;
+    button.addEventListener("click", handleChoiceClick);
+    choicesElement.appendChild(button);
+  });
+}
+
+function handleChoiceClick(event) {
+  if (answerSubmitted) {
+    return; // Prevent multiple clicks before moving to the next question
   }
 
-  const includeLowercase = confirm("Do you need lowercase letters?");
-  const includeCapital = confirm("Do you need capital letters?");
-  const includeNumbers = confirm("Do you need numbers?");
-  const includeSpecial = confirm("Do you need special characters?");
+  answerSubmitted = true; // Mark answer as submitted
+  const selectedChoiceIndex = event.target.dataset.answerIndex;
+  const currentQuestion = quizData[currentQuestionIndex];
 
-  const password = generatePassword(passwordLength, includeLowercase, includeCapital, includeNumbers, includeSpecial);
+  if (selectedChoiceIndex == currentQuestion.correctAnswer) {
+    score += 10; // Increase score by 10 for correct answer
+    event.target.classList.add("correct");
+  } else {
+    event.target.classList.add("incorrect");
+    timeLeft -= 5; // Decrement timer by 5 seconds for incorrect answer
+  }
 
-  if (password !== null) {
-    passwordText.value = password;
+  setTimeout(() => {
+    answerSubmitted = false; // Reset answerSubmitted after 2 seconds
+    event.target.classList.remove("correct", "incorrect");
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < quizData.length) {
+      showQuestion();
+      if (currentQuestionIndex === 1) {
+        startQuizTimer(); // Start the timer after the first question is answered
+      }
+    } else {
+      showResult();
+    }
+  }, 2000);
+
+  scoreElement.textContent = `Score: ${score}`; // Update score display
+}
+
+
+function startQuizTimer() {
+  if (!timerRunning) {
+    timerRunning = true;
+    timer = setInterval(function() {
+      timeLeft--;
+      timerElement.textContent = formatTime(timeLeft);
+
+      if (timeLeft === 0) {
+        clearInterval(timer);
+        showResult();
+      }
+    }, 1000);
   }
 }
 
-function generatePassword(length, includeLowercase, includeCapital, includeNumbers, includeSpecial) {
-  let charset = "";
-
-  if (includeLowercase) {
-    charset += "qwertyuiopasdfghjklzxcvbnm";
-  }
-
-  if (includeCapital) {
-    charset += "QWERTYUIOPASDFGHJKLZXCVBNM";
-  }
-
-  if (includeNumbers) {
-    charset += "1234567890";
-  }
-
-  if (includeSpecial) {
-    charset += "!@#$%^&*()";
-  }
-
-  if (charset === "") {
-    alert("No options selected.");
-    return null;
-  }
-
-  let password = "";
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset.charAt(randomIndex);
-  }
-
-  return password;
+function formatTime(time) {
+  const minutes = Math.floor(time / 60).toString().padStart(2, "0");
+  const seconds = (time % 60).toString().padStart(2, "0");
+  return `${minutes}:${seconds}`;
 }
 
-function copyPassword() {
-  passwordText.select();
-  document.execCommand("copy");
-  alert("Password copied to the clipboard");
+function showResult() {
+  clearInterval(timer);
+  choicesElement.innerHTML = "";
+
+  const playerName = prompt("Enter your name:");
+
+  if (score > 0) {
+    highScores.push({ name: playerName, score: score });
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(10);
+
+    const highScoresElement = document.getElementById("high-scores");
+    let scoreList = "<h2>Top Ten High Scores</h2>";
+    scoreList += "<ol>";
+
+    for (let i = 0; i < highScores.length; i++) {
+      scoreList += `<li>${i + 1}. ${highScores[i].name}: ${highScores[i].score}</li>`;
+    }
+
+    scoreList += "</ol>";
+    highScoresElement.innerHTML = scoreList;
+    highScoresElement.style.display = "block";
+
+    // Save high scores to local storage
+    const highScoresJson = JSON.stringify(highScores);
+    localStorage.setItem("highScores", highScoresJson);
+  }
+
+  // Hide elements
+  document.getElementById("quiz-container").style.display = "none";
+  document.getElementById("score-box").style.display = "none";
+  document.getElementById("timer-box").style.display = "none";
 }
 
-generateBtn.addEventListener("click", writePassword);
-copyBtn.addEventListener("click", copyPassword);
+// Retrieve high scores from local storage
+function loadHighScores() {
+  const highScoresJson = localStorage.getItem("highScores");
+  highScores = JSON.parse(highScoresJson) || [];
+}
+
+
+function initializeQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  timeLeft = 60;
+  answerSubmitted = false;
+  showQuestion();
+}
+
+function startTimer() {
+  timer = setInterval(function() {
+    timeLeft--;
+    timerElement.textContent = formatTime(timeLeft);
+
+    if (timeLeft === 0) {
+      clearInterval(timer);
+      showResult();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60).toString().padStart(2, "0");
+  const seconds = (time % 60).toString().padStart(2, "0");
+  return `${minutes}:${seconds}`;
+}
+
+initializeQuiz();
+loadHighScores();
